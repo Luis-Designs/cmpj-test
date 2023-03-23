95893654b1a011817cb850e62a0f9a738e62b30a
luis-designs <luislozano120xd@gmail.com>
2023-03-23 09:35:06 -0500

    :tada: First commit

---
+++ src/api/cmpj/controllers/cmpj.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cmpj controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cmpj.cmpj');
---
+++ src/api/cmpj/routes/cmpj.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cmpj router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cmpj.cmpj');
